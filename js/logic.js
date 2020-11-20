1c9ded7b7ee42150aacec1ae68806327c73032c0
Rahul Sonwanshi <rahulsonwanshi3@gmail.com>
2020-11-20 21:48:33 +0530

    revised changes

--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,18 @@
 
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
+// Event Handling Functions
+
+function handleDragStart(e) { // When starting to drag
+    this.style.opacity = '0.4'; // give visual effect of dropping it!
 
     e.dataTransfer.setData("text", e.target.id);
 }
 
-function handleDragEnd(e) {
+function handleDragEnd(e) { // When Drag has been completed meaning it has been dropped
     this.style.opacity = '1';
 }
 
 
-function handleDragOver(e) {
+function handleDragOver(e) { // drag over what area that is the rectangular area in this case
     if (e.preventDefault) {
         e.preventDefault();
     }
@@ -18,18 +20,17 @@ function handleDragOver(e) {
     return false;
 }
 
-function handleDrop(e) {
-    // e.stopPropagation(); // stops the browser from redirecting.
-    e.preventDefault();
+function handleDrop(e) { // Actually Appending the div on drop!
+    e.preventDefault(); // prevent default behaviour
     var data = e.dataTransfer.getData("text");
-    let dropBox = document.querySelector('.drop__boxes__container');
-    // e.target.appendChild(document.getElementById(data));
-    dropBox.appendChild(document.getElementById(data));
+    let dropBox = document.querySelector('.drop__boxes__container'); // dropping area
+    
+    dropBox.appendChild(document.getElementById(data)); // appendding dropped item.
     var element = document.getElementById(data);
-    element.removeEventListener('dragstart', handleDragStart);
-    element.draggable = false;
+    element.removeEventListener('dragstart', handleDragStart); // remove drag event listner so that it cannot be re-dropped on the same area
+    element.draggable = false; // to prevent re-dropping
 
-    var logDiv = document.querySelector(".action__log");
+    var logDiv = document.querySelector(".action__log"); // selecting action log to print what box was dropped!
     var pNode = document.createElement("h1");
     var node = document.createTextNode("Box " + data.replace(/^box-/, '') +" has been inserted");
     
@@ -37,18 +38,21 @@ function handleDrop(e) {
     logDiv.appendChild(pNode);
     console.log(data);
 
-    var emptyBox = document.querySelector("#empty-box-"+ data.replace(/^box-/, ''));
+    var emptyBox = document.querySelector("#empty-box-"+ data.replace(/^box-/, '')); // replace the empty space with the same empty div with different opacity
     emptyBox.classList.add("empty__box");
 }
 
+// Adding Event Listners to the Elements
+
 let items = document.querySelectorAll('.square__box');
 
-items.forEach(function(item) {
+items.forEach(function(item) { // adding event listner to square boxes for dragging
     item.addEventListener('dragstart', handleDragStart, false);
     item.addEventListener('dragend', handleDragEnd, false);
     
 });
 
+// adding event listner to dropping area for dropping
 let dropBox = document.querySelector('.drop__boxes__container');
 
 dropBox.addEventListener('drop', handleDrop, false);
---
+++ readME.md
@@ -0,0 +1,29 @@
+# Drag And Drop Demo
+
+This is a demo application for drag and drop!
+
+## Installation
+
+just use this command
+```bash
+    git clone https://github.com/rahul-sonwanshi/drag-n-drop-repo
+```
+
+## Usage
+
+On Phone Please Hold Press to do Drag and Drop.
+
+Just click on index.html
+OR
+Simply go to this url It is already Hosted Here.
+
+[CLICK HERE TO GO TO WEBSITE](https://rahul-sonwanshi.github.io/drag-n-drop-repo/index.html)
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
